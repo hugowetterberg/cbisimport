f4d215c7f57c63565464d9433aeca83057b81ac1
Hugo Wetterberg <hugo.wetterberg@gmail.com>
2010-02-26 23:35:58 +0100

    Pretty close to final import

--- cbisimport_cli.php
+++ cbisimport_cli.php
@@ -35,29 +35,6 @@ foreach ($update_status as $id => $updated) {
 variable_set('cbisimport_last_updated', $last_update);
 
 while (db_result(db_query("SELECT COUNT(pid) FROM {cbisimport_queue} WHERE fail_count=0"))) {
-  print "Getting a batch of ten products\n";
-  $res = db_query_range("SELECT * FROM {cbisimport_queue} WHERE fail_count=0", 0, 10);
-  $success = array();
-  $fail = array();
-  while ($qi = db_fetch_array($res)) {
-    $product = CbisTemplate::sanitize(cbisimport_get_product($qi['pid']));
-    if ($product && !empty($product['Name'])) {
-      print "{$product['Name']}\n";
-      cbisimport_product_recieved($product);
-      $success[] = $qi['pid'];
-    }
-    else {
-      $fail[] = $qi['pid'];
-    }
-  }
-  if (!empty($success)) {
-    print "Success " . join($success, ', ') . "\n";
-    $plc = db_placeholders($success);
-    db_query("DELETE FROM {cbisimport_queue} WHERE pid IN ({$plc})", $success);
-  }
-  if (!empty($fail)) {
-    print "Fail " . join($fail, ', ') . "\n";
-    $plc = db_placeholders($fail);
-    db_query("UPDATE {cbisimport_queue} SET fail_count = fail_count + 1 WHERE pid IN ({$plc})", $fail);
-  }
+  print "Running cbisimport cron\n";
+  cbisimport_cron(TRUE);
 }
